92cb0bcba4a2be96c14983682333e4c630af4038
Affan Mehmood <affanmehmood32@gmail.com>
2021-09-11 18:43:40 +0500

    Updates

---
+++ precache-manifest.b9ea747eff2e19e8de42eed9b8aeaaf1.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a012e07b7ff896c3090c83c86203e85",
+    "url": "/SolHistory/index.html"
+  },
+  {
+    "revision": "d3c28681618b001b7abe",
+    "url": "/SolHistory/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "edfc483c095f76bcd134",
+    "url": "/SolHistory/static/css/main.46d607c9.chunk.css"
+  },
+  {
+    "revision": "d3c28681618b001b7abe",
+    "url": "/SolHistory/static/js/2.6419ada3.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/SolHistory/static/js/2.6419ada3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edfc483c095f76bcd134",
+    "url": "/SolHistory/static/js/main.2b832346.chunk.js"
+  },
+  {
+    "revision": "0393c886384606062142",
+    "url": "/SolHistory/static/js/runtime-main.0a46fad3.js"
+  },
+  {
+    "revision": "e85010167f3c1933331ddd697b0b4fa6",
+    "url": "/SolHistory/static/media/frmenh.e8501016.png"
+  },
+  {
+    "revision": "a7dc2ab082cec75cadc4dd6f64f1df1b",
+    "url": "/SolHistory/static/media/logo.a7dc2ab0.svg"
+  },
+  {
+    "revision": "19c262faa5cb12fef81ab34b2abf86db",
+    "url": "/SolHistory/static/media/main-design.19c262fa.gif"
+  },
+  {
+    "revision": "93a5a2919888e12e2de5c317694ad938",
+    "url": "/SolHistory/static/media/objidf.93a5a291.png"
+  },
+  {
+    "revision": "b70932ab6752e9d2954e581eabbb159c",
+    "url": "/SolHistory/static/media/objlb.b70932ab.png"
+  },
+  {
+    "revision": "95a830ae03586e302ea0cecdda797605",
+    "url": "/SolHistory/static/media/objrec.95a830ae.png"
+  },
+  {
+    "revision": "85b414d47f70cdc3ef6379ebea26a74c",
+    "url": "/SolHistory/static/media/rptgen.85b414d4.png"
+  },
+  {
+    "revision": "6be53f373ecfd65f72294d69c381176a",
+    "url": "/SolHistory/static/media/solana-gradient.6be53f37.svg"
+  },
+  {
+    "revision": "893f060392de180b50a716b157c7a929",
+    "url": "/SolHistory/static/media/usrfrn.893f0603.png"
+  }
+]);
